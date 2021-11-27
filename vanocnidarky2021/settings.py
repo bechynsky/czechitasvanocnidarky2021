667a4c04f01b9b116e2aa26363d692f784f60b52
Iva Fischerová <iva.fischerova@avast.com>
2021-11-27 17:52:31 +0100

    updated styles and pics

--- vanocnidarky2021/settings.py
+++ vanocnidarky2021/settings.py
@@ -140,8 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app_vanocnidarky2021/static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
